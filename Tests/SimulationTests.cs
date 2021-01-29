093920c567709929c515c5049aacc20df52d5114
Nick Jones <nijon@microsoft.com>
2021-01-29 12:59:08 -0800

    rearranging some tests

--- Tests/SimulationTests.cs
+++ Tests/SimulationTests.cs
@@ -10,9 +10,28 @@ namespace TI4BattleSim
     {
 
         [TestMethod]
+        public void SpaceCannonOffenseScenario()
+        {
+            // verifies that space cannon offense occurs as expected
+            List<double> sims = Scenarios.SpaceCannonOffenseSim();
+            List<double> predicted = new List<double>() { 0.347, 0.6059, 0.0471 };
+            AssertWithinTolerances(predicted, sims);
+        }
+
+        [TestMethod]
+        public void SpaceCannonOffenseThirdPartyScenario()
+        {
+            // verifies that space cannon offense occurs as expected with "third parties"
+            List<double> sims = Scenarios.SpaceCannonOffenseThirdPartySim();
+            List<double> predicted = new List<double>() { 0.347, 0.6059, 0.0471 };
+            AssertWithinTolerances(predicted, sims);
+        }
+
+        [TestMethod]
         public void ArgentDestroyersScenario()
         {
             //verifies argent destroyers function as expected
+            //todo: verify capacity concerns + mech as well
             List<double> sims = Scenarios.ArgentFlightDestroyerSim();
             List<double> predicted = new List<double>() { 0.6762, 0.2909, 0.0329 };
             AssertWithinTolerances(predicted, sims);
@@ -25,24 +44,6 @@ namespace TI4BattleSim
             //todo: verify capacity concerns as well
             List<double> sims = Scenarios.TitansCruiserSim();
             List<double> predicted = new List<double>() { 0.6316, 0.3319, 0.0365 };
-            AssertWithinTolerances(predicted, sims);
-        }
-
-        [TestMethod]
-        public void GenericMechScenario()
-        {
-            //verifies that mechs perform as expected
-            List<double> sims = Scenarios.GenericMechSim();
-            List<double> predicted = new List<double>() { 0.407, 0.593, 0.00 };
-            AssertWithinTolerances(predicted, sims);
-        }
-
-        [TestMethod]
-        public void MixedGenericGroundCombatScenario()
-        {
-            //verifies that mechs perform as expected
-            List<double> sims = Scenarios.MixedGenericGroundCombatSim();
-            List<double> predicted = new List<double>() { 0.4668, 0.5332, 0.00 };
             AssertWithinTolerances(predicted, sims);
         }
 
@@ -83,24 +84,6 @@ namespace TI4BattleSim
         }
 
         [TestMethod]
-        public void SpaceCannonOffenseScenario()
-        {
-            // verifies that space cannon offense occurs as expected
-            List<double> sims = Scenarios.SpaceCannonOffenseSim();
-            List<double> predicted = new List<double>() { 0.347, 0.6059, 0.0471 };
-            AssertWithinTolerances(predicted, sims);
-        }
-
-        [TestMethod]
-        public void SpaceCannonOffenseThirdPartyScenario()
-        {
-            // verifies that space cannon offense occurs as expected with "third parties"
-            List<double> sims = Scenarios.SpaceCannonOffenseThirdPartySim();
-            List<double> predicted = new List<double>() { 0.347, 0.6059, 0.0471 };
-            AssertWithinTolerances(predicted, sims);
-        }
-
-        [TestMethod]
         public void PlanetaryShieldPDSScenario()
         {
             // verifies pds blocks generic bombardment
@@ -115,6 +98,24 @@ namespace TI4BattleSim
             // verifies war suns "break" planetary shield
             List<double> sims = Scenarios.WarSunBreaksPlanetaryShieldSim();
             List<double> predicted = new List<double>() { 0.4184, 0.5816, 0.00 };
+            AssertWithinTolerances(predicted, sims);
+        }
+
+        [TestMethod]
+        public void GenericMechScenario()
+        {
+            //verifies that mechs perform as expected
+            List<double> sims = Scenarios.GenericMechSim();
+            List<double> predicted = new List<double>() { 0.407, 0.593, 0.00 };
+            AssertWithinTolerances(predicted, sims);
+        }
+
+        [TestMethod]
+        public void MixedGenericGroundCombatScenario()
+        {
+            //verifies that mechs perform as expected
+            List<double> sims = Scenarios.MixedGenericGroundCombatSim();
+            List<double> predicted = new List<double>() { 0.4668, 0.5332, 0.00 };
             AssertWithinTolerances(predicted, sims);
         }
 
